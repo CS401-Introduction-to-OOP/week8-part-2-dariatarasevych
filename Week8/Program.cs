8714f7fa8c2afb570983d9f88dd6a1e3136c9239
daratarasevic <tarasevish80@gmail.com>
2026-03-28 17:36:14 +0200

    Stage 1

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,25 +1,9 @@
-﻿using Week8;
+﻿
 class Program
 {
     public static void Main()
     {
-        var file = new FileResource("report.txt");
-        var network = new NetworkResource("api.company.local");
-        var manager = new ResourceManager<Resource>();
-         //1. Add resources to manager
-        manager.Add(file);
-        manager.Add(network);
-        //2. Open all resources
-        manager.OpenAll();
         
-        Console.WriteLine("3. Demonstrate using with one disposable resource");
-        using (var someFile = new FileResource("someFile.txt"))
-        {
-            someFile.Open();
-        }
-        
-        Console.WriteLine("4. Close all resources");
-        manager.CloseAll();
     }
     
 }
