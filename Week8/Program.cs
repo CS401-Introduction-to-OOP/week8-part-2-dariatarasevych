76a4119c65b28b2c2a9f4fa2fc5280dde448947b
daratarasevic <tarasevish80@gmail.com>
2026-03-28 17:53:20 +0200

    Stage 3

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,27 @@
-﻿
+﻿using Week8;
 class Program
 {
     public static void Main()
     {
+        // Value type demo
+        Point p1 = new Point { X = 1, Y = 2 };
+        Point p2 = p1;
+
+        p2.X = 5;
+        p1.Print();
+        p2.Print();
+        Console.WriteLine();
         
+// Reference type demo
+        PointRef r1 = new PointRef { X = 1, Y = 2 };
+        PointRef r2 = r1;
+
+        r2.X = 9;
+        r1.Print();
+        r2.Print();
+        Console.WriteLine();
+// Boxing demo
+        BoxingTester.Test();
     }
     
 }
