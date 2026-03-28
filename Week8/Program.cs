8c4f8b5ff6bbc1ccc40e2831d023991ef6293b55
daratarasevic <tarasevish80@gmail.com>
2026-03-28 17:26:42 +0200

    Stage 3

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -6,9 +6,10 @@ class Program
         var file = new FileResource("report.txt");
         var network = new NetworkResource("api.company.local");
         var manager = new ResourceManager<Resource>();
-        
+         //1. Add resources to manager
         manager.Add(file);
         manager.Add(network);
+        //2. Open all resources
         manager.OpenAll();
         
         Console.WriteLine("3. Demonstrate using with one disposable resource");
